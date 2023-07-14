0e75bbacffffed7af80d5f1f0945b49e891ccb50
oussama <oussamamrkd2@gmail.com>
2023-07-14 21:09:42 +0100

    Fix man js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const WebpackObfuscator = require('webpack-obfuscator');
 
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
+  devtool: false,
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'ezajil-sdk.min.js',
@@ -25,5 +27,14 @@ module.exports = {
   optimization: {
   },
   plugins: [
+    new WebpackObfuscator ({
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 1,
+        numbersToExpressions: true,
+        stringArrayShuffle: true,
+        splitStrings: true,
+        stringArrayThreshold: 1
+    }, [])
   ],
 };
